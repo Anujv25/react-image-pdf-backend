7616decc8669e1bac26acac2784e429e2fe7824c
Anuj Verm <anuj.vgithub25@gmail.com>
2025-02-15 16:03:18 +0530

    changes for GET api

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -6,5 +6,8 @@ const router = Router();
 const upload = multer({ storage: memoryStorage() });
 
 router.post("/convert-to-pdf", upload.array("images", 10), convertImagesToPDF);
+router.get("/", (req, res) => {
+    res.send("Welcome to PDF Converter API");
+})
 
 export {router as psdRouter};
