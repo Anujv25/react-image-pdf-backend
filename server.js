c30ed2ba6308bb2d59f62318e1569e665a975306
Anuj Verm <anuj.vgithub25@gmail.com>
2025-02-16 16:33:03 +0530

    changes for payload size

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 const port = 3000;
 app.use(express.json()); // Parse JSON request bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+// Increase payload size limit
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors());
 app.use("/api", psdRouter);
